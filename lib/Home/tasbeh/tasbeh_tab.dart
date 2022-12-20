110c2d2f1f63ddf548e4af20d1c0388696d62829
Esraa <esraa1995.ei@gmail.com>
2022-12-20 12:24:22 +0200

    update tasbeh tab

--- lib/Home/tasbeh/tasbeh_tab.dart
+++ lib/Home/tasbeh/tasbeh_tab.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/settings_provider.dart';
 
 class TasbehTab extends StatefulWidget {
   @override
@@ -9,43 +12,107 @@ class TasbehTab extends StatefulWidget {
 
 class _TasbehTabState extends State<TasbehTab> {
   int Counter = 0;
+  double angle = 0;
+
+  int currentIndex = 0;
+  List<String> azkarTitle = [
+    'سبحان الله',
+    'الحمد الله',
+    ' الله اكبر',
+    'استغفر الله',
+  ];
+
+  void changeAngle() {
+    angle--;
+    // Counter++;
+    // if(Counter == 34){
+    //   currentIndex++;
+    //   Counter = 0;
+    // }
+    // if(currentIndex > azkarTitle.length-1){
+    //   currentIndex=0;
+    // }
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
+    var settingProvider = Provider.of<SettingsProvider>(context);
     return Container(
+      width: double.infinity,
       alignment: Alignment.center,
       child: Column(
         children: [
-          Image.asset('assets/images/head_sebha_logo.png'),
-          Image.asset('assets/images/body_sebha_logo.png'),
+          Container(
+              height: MediaQuery.of(context).size.height * 0.46,
+              child: Stack(
+                children: [
+                  Positioned(
+                      left: MediaQuery.of(context).size.width * .48,
+                      child: Image.asset(settingProvider.isDarkMode()
+                          ? 'assets/images/head_sebha_dark.png'
+                          : 'assets/images/head_sebha_logo.png')),
+                  Positioned(
+                      top: 78,
+                      left: MediaQuery.of(context).size.width * 0.20,
+                      child: InkWell(
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        onTap: () {
+                          changeAngle();
+                        },
+                        child: Transform.rotate(
+                          angle: angle,
+                          child: Image.asset(settingProvider.isDarkMode()
+                              ? 'assets/images/body_sebha_dark.png'
+                              : 'assets/images/body_sebha_logo.png'),
+                        ),
+                      )),
+                ],
+              )),
           Container(
             margin: EdgeInsets.symmetric(vertical: 15),
             child: Text(
               AppLocalizations.of(context)!.tasbehat_number,
-              style: TextStyle(fontSize: 24),
+              style: (Theme.of(context).textTheme.headline4),
             ),
           ),
           Container(
+            width: 60,
+            height: 60,
+            alignment: Alignment.center,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.amberAccent),
+                borderRadius: BorderRadius.circular(15),
+                color: Theme.of(context).primaryColor),
             padding: EdgeInsets.all(15),
             child: Text(
               Counter.toString(),
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              style: Theme.of(context).textTheme.headline6,
             ),
           ),
+          SizedBox(
+            height: 10,
+          ),
           Container(
-            margin: EdgeInsets.only(top: 10),
+            padding: EdgeInsets.symmetric(horizontal: 15),
             child: ElevatedButton(
               onPressed: () {
-                setState(() {
-                  Counter++;
-                });
+                Counter++;
+                if (Counter == 35) {
+                  currentIndex++;
+                  Counter = 0;
+                }
+                if (currentIndex > azkarTitle.length - 1) {
+                  currentIndex = 0;
+                }
+                setState(() {});
               },
-              child: Text('سبحان الله'),
+              child: Text(
+                azkarTitle[currentIndex],
+                style: Theme.of(context).textTheme.headline6,
+              ),
               style: ElevatedButton.styleFrom(
-                  primary: Theme.of(context).primaryColor,
+                  primary: Theme.of(context).accentColor,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(30))),
             ),
